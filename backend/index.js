8522fdc5e99adbdcba8812f9b206debd1d9cb032
Ankit BagheL <ak9664773@gmail.com>
2023-06-01 12:58:43 +0530

    signup page data post to DB and Connect node with db

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
-const express=require('express');
-const app=express();
-
-app.get('/',(req,resp)=>{
-    resp.send('<h1>HEllo world</h1>')
-})
+const express = require('express');
+require('./db/config');
+const User = require('./db/User');
+const app = express();
+const cors=require('cors');
+app.use(cors());
+app.use(express.json());
+app.post("/register",async (req, resp) => {
+    let user=new User(req.body);
+    let result=await user.save();
+    resp.send(req.body);
+    // console.log(req.body);
+});
 
 app.listen(5000);
--- front-end/src/components/SignUp.jsx
+++ front-end/src/components/SignUp.jsx
@@ -1,22 +1,43 @@
-import React,{useState} from 'react'
-const SignUp=()=>{
+import React, { useState } from 'react';
+import {useNavigate} from 'react-router-dom';
+
+const SignUp = () => {
     const [name, setname] = useState("");
     const [email, setemail] = useState("");
     const [password, setpassword] = useState("");
-    const collectData=()=>{
-        console.log(name);
-        console.log(email);
-        console.log(password);
+    const navigate=useNavigate();
+
+    const collectData = () => {
+        console.log(name, email, password);
+
+        const collectData = async () => {
+            let result = await fetch('http://localhost:5000/register', {
+                method: 'post',
+                body: JSON.stringify({ name, email, password }),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            result = await result.json();
+            console.warn(result);
+
+            if(result)
+                navigate('/');
+
+        }
+
+        collectData();
+        
         setname("");
         setemail("");
         setpassword("");
     }
-    return(
-        <div className='register'> 
+    return (
+        <div className='register'>
             <h1>Register</h1>
-            <input className='inputBox' type="text" value={name} onChange={(e)=>{setname(e.target.value)}} placeholder='Enter Your Name' />
-            <input className='inputBox' type="email" value={email} onChange={(e)=>{setemail(e.target.value)}} placeholder='Enter Your Email' />
-            <input className='inputBox' type="password" value={password} onChange={(e)=>{setpassword(e.target.value)}} placeholder='Enter Your Password' />
+            <input className='inputBox' type="text" value={name} onChange={(e) => { setname(e.target.value) }} placeholder='Enter Your Name' />
+            <input className='inputBox' type="email" value={email} onChange={(e) => { setemail(e.target.value) }} placeholder='Enter Your Email' />
+            <input className='inputBox' type="password" value={password} onChange={(e) => { setpassword(e.target.value) }} placeholder='Enter Your Password' />
             <button className='appButton' type='button' onClick={collectData}>Sign Up</button>
         </div>
     )
